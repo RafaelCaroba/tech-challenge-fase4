50967bc34ca7992cd45e511f7aa47f9f50a44ee6
Rafael Caroba de Souza <rafa.caroba.17@gmail.com>
2026-05-06 00:18:11 -0300

    Commit inicial

---
+++ src/main/java/com/caroba/techchallenge/feedback/function/FeedBackController.java
@@ -0,0 +1,20 @@
+package com.caroba.techchallenge.feedback.function;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/avaliacao")
+public class FeedBackController {
+
+//    private final FeedbackFuncion funcion;
+
+//    public FeedbackController(FeedbackFunction function) {
+//        this.function = function;
+//    }
+//
+//    @PostMapping
+//    public String avaliar(@RequestBody FeedbackRequest request) {
+//        return function.apply(request);
+//    }
+}
